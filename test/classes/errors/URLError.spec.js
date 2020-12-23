165dbf464e6f5f083edf6cf830d306bc3154758d
moonstar-x <christian.lopez99@outlook.com>
2020-12-23 17:07:45 -0500

    Created URLError class.

---
+++ test/classes/errors/URLError.spec.js
@@ -0,0 +1,24 @@
+import URLError from '../../../src/classes/errors/URLError';
+
+describe('Classes - Errors - URLError', () => {
+  it('should be instance of Error.', () => {
+    expect(new URLError()).toBeInstanceOf(Error);
+  });
+
+  it('should have URLError name property.', () => {
+    expect(new URLError().name).toBe('URLError');
+  });
+
+  it('should contain a message.', () => {
+    const message = 'Oops';
+    expect(new URLError(message).message).toBe(message);
+  });
+
+  it('should have a stack trace.', () => {
+    try {
+      throw new URLError('Oops');
+    } catch (err) {
+      expect(err.stack).toBeTruthy();
+    }
+  });
+});
