7ea937cb83f8810c54aad116b14230ab76a30c4b
moonstar-x <christian.lopez99@outlook.com>
2020-12-24 15:50:29 -0500

    Created VoiceChannelError class

---
+++ test/classes/errors/VoiceChannelError.spec.js
@@ -0,0 +1,24 @@
+import VoiceChannelError from '../../../src/classes/errors/VoiceChannelError';
+
+describe('Classes - Errors - VoiceChannelError', () => {
+  it('should be instance of Error.', () => {
+    expect(new VoiceChannelError()).toBeInstanceOf(Error);
+  });
+
+  it('should have VoiceChannelError name property.', () => {
+    expect(new VoiceChannelError().name).toBe('VoiceChannelError');
+  });
+
+  it('should contain a message.', () => {
+    const message = 'Oops';
+    expect(new VoiceChannelError(message).message).toBe(message);
+  });
+
+  it('should have a stack trace.', () => {
+    try {
+      throw new VoiceChannelError('Oops');
+    } catch (err) {
+      expect(err.stack).toBeTruthy();
+    }
+  });
+});
