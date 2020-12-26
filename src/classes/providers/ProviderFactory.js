e606e714d8428a55ca5bf01ec93f5ec5da1c2b4e
moonstar-x <christian.lopez99@outlook.com>
2020-12-26 17:01:38 -0500

    Added support for SoundCloud

--- src/classes/providers/ProviderFactory.js
+++ src/classes/providers/ProviderFactory.js
@@ -1,12 +1,18 @@
 import YouTubeProvider from '../providers/YouTubeProvider';
+import SoundCloudProvider from '../providers/SoundCloudProvider';
 import URLError from '../errors/URLError';
 
 const youtubeProvider = new YouTubeProvider();
+const soundCloudProvider = new SoundCloudProvider();
 
 class ProviderFactory {
   static getInstance(url) {
     if (url.includes('youtube.com') || url.includes('youtu.be')) {
       return youtubeProvider;
+    }
+
+    if (url.includes('soundcloud.com')) {
+      return soundCloudProvider;
     }
     
     throw new URLError(`Invalid URL in queue: ${url}`);
