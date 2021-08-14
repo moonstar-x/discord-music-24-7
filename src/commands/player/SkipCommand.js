1fcad089d8eb4521ffbda8daf2a1a91546e5b6d2
moonstar-x <christian.lopez99@outlook.com>
2021-08-14 01:00:27 -0500

    Skip command now checks member channel.

--- src/commands/player/SkipCommand.js
+++ src/commands/player/SkipCommand.js
@@ -13,6 +13,10 @@ class SkipCommand extends Command {
   }
 
   run(message) {
+    if (message.member.voice.channel.id !== this.client.player.channel.id) {
+      return message.reply(`You need to be in ${this.client.player.channel} in order to skip the current song.`);
+    }
+
     this.client.player.skipCurrentSong(`User ${message.member.displayName} has skipped the current song.`);
     
     return message.channel.send('Song has been skipped!');
